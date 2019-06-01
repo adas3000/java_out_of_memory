feb7eedd3b23ea84e189bb9c2d60a0f6520af36e
Adam <38322422+adas3000@users.noreply.github.com>
2019-06-01 14:41:02 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world ! ");
+
+        long array[][];
+
+        while (true) {
+        array = new long [1024*1024][1024*1024];
+        }
+
+    }
+
+}
